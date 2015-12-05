0faaa5feca95c44b09a93f747d72c3614eddeb32
Robert Stoll <rstoll@tutteli.ch>
2015-12-05 10:07:14 +0100

    reformatted code and added use strict

--- src/pre-work.js
+++ src/pre-work.js
@@ -5,65 +5,68 @@
  */
 
 (function(){
-    angular.module('tutteli.preWork', [])
-        .directive('preWork', PreWorkDirective)
-        .service('tutteli.PreWork', PreWork);
-    
-    var preparedData = {};
-    
-    function PreWork(){
-        this.merge = function(name, scope, controllerAs) {
-            if(preparedData[name] == undefined || controllerAs && preparedData[name][controllerAs] == undefined) {
-                return false;
-            }
-            
-            var data = preparedData[name];
-            if (controllerAs && data[controllerAs]) {
-                data = data[controllerAs];
-            }
-            
-            for (var prop in data) {
-                scope[prop] = data[prop];
-            }
-            
-            if (controllerAs) {
-                delete preparedData[name][controllerAs];   
-            } else {
-                delete preparedData[name];
-            }
-       
-            return true;
-        };
+'use strict';
+
+angular.module('tutteli.preWork', [])
+    .directive('preWork', PreWorkDirective)
+    .service('tutteli.PreWork', PreWork);
+
+var preparedData = {};
+
+function PreWork(){
+    this.merge = function(name, scope, controllerAs) {
+        if(preparedData[name] == undefined || controllerAs && preparedData[name][controllerAs] == undefined) {
+            return false;
+        }
         
-        this.get = function(name, controllerAs) {
-            var data = preparedData[name]; 
-            if (data && controllerAs) {
-                return data[controllerAs];
-            }
-            return data;
-        };
-    }
+        var data = preparedData[name];
+        if (controllerAs && data[controllerAs]) {
+            data = data[controllerAs];
+        }
+        
+        for (var prop in data) {
+            scope[prop] = data[prop];
+        }
+        
+        if (controllerAs) {
+            delete preparedData[name][controllerAs];   
+        } else {
+            delete preparedData[name];
+        }
+   
+        return true;
+    };
     
-    PreWorkDirective.$inject = ['$parse', '$templateCache'];
-    function PreWorkDirective($parse, $templateCache) {
-        return {
-            scope:{},
-            compile: function (tElement, tAttrs, transclude) {
-                var name = tAttrs.preWork;
-                if ($templateCache.get(name) ==  undefined) {
-                    $templateCache.put(name, tElement.html());
-                    preparedData[name] = {};
-                    angular.forEach(tElement[0].querySelectorAll('input[ng-model]'), function(tInput){
-                        var input = angular.element(tInput);
-                        var model = $parse(input.attr('ng-model'));
-                        model.assign(preparedData[name], input.val());
-                    });
-                    tElement.remove();
-                }
-                return function link(scope, elem, attrs) {
-                    //nothing to do anymore
-                }; 
+    this.get = function(name, controllerAs) {
+        var data = preparedData[name]; 
+        if (data && controllerAs) {
+            return data[controllerAs];
+        }
+        return data;
+    };
+}
+
+PreWorkDirective.$inject = ['$parse', '$templateCache'];
+function PreWorkDirective($parse, $templateCache) {
+    return {
+        scope:{},
+        compile: function (tElement, tAttrs, transclude) {
+            var name = tAttrs.preWork;
+            if ($templateCache.get(name) ==  undefined) {
+                $templateCache.put(name, tElement.html());
+                preparedData[name] = {};
+                angular.forEach(tElement[0].querySelectorAll('input[ng-model]'), function(tInput){
+                    var input = angular.element(tInput);
+                    var model = $parse(input.attr('ng-model'));
+                    model.assign(preparedData[name], input.val());
+                });
+                tElement.remove();
             }
-        };
-    }    
+            return function link(scope, elem, attrs) {
+                //nothing to do anymore
+            }; 
+        }
+    };
+}    
+
 })();
